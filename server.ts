8076bcb0df737df04becffdbda614c23d43600b4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-07-09 20:41:51 +0100

    static file server

--- server.ts
+++ server.ts
@@ -1,3 +1,12 @@
+import { serveFile } from "https://deno.land/std@0.58.0/http/file_server.ts";
+
 Deno.serve((req: Request): Response => {
-  return new Response("Hello, World!");
+  let path;
+  if (req.url === "/") {
+    path = `${Deno.cwd()}/index.html`;
+  } else {
+    path = `${Deno.cwd()}/public${req.url}`;
+  }
+  const content = await serveFile(path);
+  return new Response(content);
 });
