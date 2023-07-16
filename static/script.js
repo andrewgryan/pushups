222f9a64b35b7e8a252c52fb700a9497c735c2db
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-07-16 22:09:32 +0100

    better style

---
+++ static/script.js
@@ -0,0 +1,69 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabase = createClient(
+  "https://kixpbxytlkwqauqsckme.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpeHBieHl0bGt3cWF1cXNja21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5MzIxNTksImV4cCI6MjAwNDUwODE1OX0.jHYoyq282s1Yhq382geJpeBhf5BGiN4ObnOPk9NBrzo"
+);
+
+let { data: workouts, errpr } = await supabase
+  .from("workouts")
+  .select("pushup_count,workout_date");
+
+// Render previous sessions
+let ul = document.getElementById("sessions");
+workouts.forEach((workout) => {
+  let date = new Date(workout.workout_date);
+  let li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode(`${date.toLocaleString()}: ${workout.pushup_count}`)
+  );
+  ul.appendChild(li);
+});
+
+// Connect event to supabase
+document.getElementById("btn").addEventListener("click", async (ev) => {
+  let el = document.getElementById("counter");
+  const count = el.value;
+  const date = new Date();
+
+  const { data, error } = await supabase
+    .from("workouts")
+    .insert([{ pushup_count: count, workout_date: date }])
+    .select();
+  console.log({ data, error });
+});
+
+// Chart.js
+import {
+  Chart,
+  registerables,
+} from "https://cdn.jsdelivr.net/npm/chart.js@4.3.0/+esm";
+Chart.register(...registerables);
+Chart.defaults.color = "white";
+Chart.defaults.borderColor = "hsl(240deg 50% 50% / 0.3)";
+
+const render = (data) => {
+  new Chart(document.getElementById("chart"), {
+    type: "line",
+    data,
+  });
+};
+
+const toLabels = (workouts) => {
+  return workouts.map((workout, i) => i);
+};
+
+const toDatasets = (workouts) => {
+  const reps = workouts.map((workout) => workout.pushup_count);
+  const sets = workouts.map((workout) => 1);
+  const total = workouts.map((workout) => workout.pushup_count);
+  return [
+    { label: "Reps", data: reps },
+    { label: "Sets", data: sets },
+    { label: "Total", data: total },
+  ];
+};
+
+render({
+  labels: toLabels(workouts),
+  datasets: toDatasets(workouts),
+});
