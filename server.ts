b1e68304038952c5d0b24d4be7def5083987b95e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-08-03 20:13:02 +0100

    form data

--- server.ts
+++ server.ts
@@ -2,9 +2,9 @@ import { serveDir } from "https://deno.land/std@0.194.0/http/file_server.ts";
 
 Deno.serve(async (req: Request): Response => {
   if (req.method === "POST") {
-    const data = await req.json();
+    const data = await req.formData();
     console.log({ data });
-    return new Response(`<h1>${data.secret}</h1>`);
+    return new Response(`<h1>${data.get("secret")}</h1>`);
   }
   return serveDir(req, { fsRoot: "./static" });
 });
