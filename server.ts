76e69d73a9ae80675445aa8b2e2515aa13b23b0a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-08-03 20:08:34 +0100

    more htmx

--- server.ts
+++ server.ts
@@ -2,7 +2,9 @@ import { serveDir } from "https://deno.land/std@0.194.0/http/file_server.ts";
 
 Deno.serve(async (req: Request): Response => {
   if (req.method === "POST") {
-    return new Response("<h1>POST</h1>");
+    const data = await request.json();
+    console.log({ data });
+    return new Response(`<h1>${data.secret}</h1>`);
   }
   return serveDir(req, { fsRoot: "./static" });
 });
