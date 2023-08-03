08f930d5c5507cc094661175072adf31bd16ab25
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-08-03 20:11:10 +0100

    fix reference

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { serveDir } from "https://deno.land/std@0.194.0/http/file_server.ts";
 
 Deno.serve(async (req: Request): Response => {
   if (req.method === "POST") {
-    const data = await request.json();
+    const data = await req.json();
     console.log({ data });
     return new Response(`<h1>${data.secret}</h1>`);
   }
