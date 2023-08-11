aca8f8b21958f583bc06aca895a93514cbd8dbda
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-08-11 15:31:06 +0200

    Update series.js

--- static/series.js
+++ static/series.js
@@ -1,4 +1,13 @@
 import "https://cdn.plot.ly/plotly-2.24.1.min.js"
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabase = createClient(
+  "https://kixpbxytlkwqauqsckme.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpeHBieHl0bGt3cWF1cXNja21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5MzIxNTksImV4cCI6MjAwNDUwODE1OX0.jHYoyq282s1Yhq382geJpeBhf5BGiN4ObnOPk9NBrzo"
+);
+
+let { data: planks, error } = await supabase
+  .from("plank")
+  .select("seconds,workout_date");
 
 const el = document.getElementById('series');
 const data = [{
