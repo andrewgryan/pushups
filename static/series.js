700c8255fa7d3feea0cc22ecf01db1fcbcaf3118
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-08-11 18:57:53 +0200

    Update series.js

--- static/series.js
+++ static/series.js
@@ -23,7 +23,7 @@ const data = [{
 	}
 }];
 const m = 25;
-const color = getComputedStyle(root).getPropertyValue("--surface-active");
+const color = "oklch(30% 0.1 220deg)" // getComputedStyle(root).getPropertyValue("--surface-active");
 const layout = {
 	  title: "Progress",
 	  plot_bgcolor: "transparent",
