cf5c52143d5db733180c6ba8208c4044e1c69c52
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-09-27 19:59:52 +0100

    remove comments

--- server.ts
+++ server.ts
@@ -17,12 +17,8 @@ app.get("/activity", async (c) => {
 app.get("/user", (c) => {
   try {
     const supabaseClient = createClient(
-      // Supabase API URL - env var exported by default.
       Deno.env.get("SUPABASE_URL") ?? "",
-      // Supabase API ANON KEY - env var exported by default.
       Deno.env.get("SUPABASE_ANON_KEY") ?? "",
-      // Create client with Auth context of the user that called the function.
-      // This way your row-level-security (RLS) policies are applied.
       {
         global: {
           headers: { Authorization: c.req.headers.get("Authorization")! },
