71f1909184ae16023a59e2e24eaf8ba2b22e3931
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-09-27 17:06:04 +0100

    experiment with Hono

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import { Hono } from "https://deno.land/x/hono/mod.ts";
 import {
   serveDir,
   serveFile,
@@ -8,52 +9,59 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpeHBieHl0bGt3cWF1cXNja21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5MzIxNTksImV4cCI6MjAwNDUwODE1OX0.jHYoyq282s1Yhq382geJpeBhf5BGiN4ObnOPk9NBrzo"
 );
 
-Deno.serve(async (req: Request): Response => {
-  if (req.method === "GET") {
-    const { pathname, searchParams } = new URL(req.url);
-    if (pathname === "/activity") {
-      const activity = searchParams.get("activity");
-      return serveFile(req, `./static/forms/${activity}.html`);
-    }
-  }
-  if (req.method === "POST") {
-    // Form data
-    const formData = await req.formData();
-    const activity = formData.get("activity");
-    let table;
-    let row;
-    const workout_date = new Date();
-    if (activity === "push-up") {
-      table = "workouts";
-      const repetitions = formData.get("repetitions");
-      const sets = formData.get("sets");
-      row = {
-        repetitions,
-        sets,
-        workout_date,
-      };
-    } else {
-      table = "plank";
-      const seconds = formData.get("seconds");
-      const variation = formData.get("variation");
-      row = {
-        seconds,
-        workout_date,
-        variation,
-      };
-    }
+const app = new Hono();
 
-    // Set row in database
-    const { data, error } = await supabase.from(table).insert([row]).select();
+app.get("/", (c) => c.text("Hello, Hono!"));
 
-    console.log({ data, error });
+Deno.serve(app.fetch);
 
-    // Return confirmation that a workout has been submitted
-    return new Response(`<h2>Submitted</h2>`, {
-      headers: {
-        "HX-Trigger": "workout-submit",
-      },
-    });
-  }
-  return serveDir(req, { fsRoot: "./static" });
-});
+// Server entrypoint
+// Deno.serve(async (req: Request): Response => {
+//   if (req.method === "GET") {
+//     const { pathname, searchParams } = new URL(req.url);
+//     if (pathname === "/activity") {
+//       const activity = searchParams.get("activity");
+//       return serveFile(req, `./static/forms/${activity}.html`);
+//     }
+//   }
+//   if (req.method === "POST") {
+//     // Form data
+//     const formData = await req.formData();
+//     const activity = formData.get("activity");
+//     let table;
+//     let row;
+//     const workout_date = new Date();
+//     if (activity === "push-up") {
+//       table = "workouts";
+//       const repetitions = formData.get("repetitions");
+//       const sets = formData.get("sets");
+//       row = {
+//         repetitions,
+//         sets,
+//         workout_date,
+//       };
+//     } else {
+//       table = "plank";
+//       const seconds = formData.get("seconds");
+//       const variation = formData.get("variation");
+//       row = {
+//         seconds,
+//         workout_date,
+//         variation,
+//       };
+//     }
+
+//     // Set row in database
+//     const { data, error } = await supabase.from(table).insert([row]).select();
+
+//     console.log({ data, error });
+
+//     // Return confirmation that a workout has been submitted
+//     return new Response(`<h2>Submitted</h2>`, {
+//       headers: {
+//         "HX-Trigger": "workout-submit",
+//       },
+//     });
+//   }
+//   return serveDir(req, { fsRoot: "./static" });
+// });
